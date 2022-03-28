93e5180a10e59551f41067581a2fbcaf1f287d94
FATMAKAMSHAD90 <fatmakamshad90@gmail.com>
2022-03-28 10:56:34 +0300

    done

--- src/App.js
+++ src/App.js
@@ -3,61 +3,9 @@ import "./App.css";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { auth } from "./firebase";
+import Req from "./Req";
 function App() {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("🚀 ~ file: App.js ~ line 8 ~ App ~ user", user);
-    createUserWithEmailAndPassword(auth, user.email, user.password)
-      .then((userCredential) => {
-        console.log(
-          "🚀 ~ file: App.js ~ line 14 ~ .then ~ userCredential",
-          userCredential
-        );
-        // Signed in
-        // const user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="form">
-          <form onSubmit={handleSubmit}>
-            <div className="input-container">
-              <label>Email </label>
-              <input
-                type="text"
-                name="email"
-                required
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-              />
-            </div>
-            <div className="input-container">
-              <label>Password </label>
-              <input
-                type="password"
-                name="pass"
-                required
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-              />
-            </div>
-            <div className="button-container" onClick={handleSubmit}>
-              <input type="submit" />
-            </div>
-          </form>
-        </div>
-      </header>
-    </div>
-  );
+  return <Req />;
 }
 
 export default App;
---
+++ src/Req.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+export default function Req() {
+  return (
+    <div>
+      <h3>Welcome To Gulf Bank!</h3>
+      <h4>First step </h4>
+      <h4>Make sure the below requirments are available</h4>
+      <Table responsive="sm">
+        <thead>
+          <tr>
+            <h3>
+              {" "}
+              List of Documents required for obtaining approval to open SME
+              account
+            </h3>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1-Company Article of Association And All deed of amendment.</td>
+          </tr>
+          <tr>
+            <td>2-Signiture Authintication for the Company </td>
+          </tr>
+          <tr>
+            <td>3-Commercial Civil Id</td>
+          </tr>
+          <tr>
+            <td>4-Comercial Registeration</td>
+          </tr>
+          <tr>
+            <td>5-Civil Id (for all partners and representitives)</td>
+          </tr>
+          <tr>
+            <td>
+              6- Authorization Certificate from Ministry of Commerce (ensure
+              that date is not more than 10 days)
+            </td>
+          </tr>
+          <tr>
+            <td>7-Commercial License</td>
+          </tr>
+          <tr>
+            <td>8- Account Statment (six months) if available</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+}
