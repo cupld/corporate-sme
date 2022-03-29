490f7c70d0c8fdca42d18447876524d6a794d9b1
feheed <fmjuwaihel@gmail.com>
2022-03-29 08:37:35 +0300

    designing website design

--- src/App.js
+++ src/App.js
@@ -1,61 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
-import { auth } from "./firebase";
+import Application from "./Components/Application";
+import NavBar from "./Components/NavBar";
+import Testing from "./Components/Testing";
 function App() {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("🚀 ~ file: App.js ~ line 8 ~ App ~ user", user);
-    createUserWithEmailAndPassword(auth, user.email, user.password)
-      .then((userCredential) => {
-        console.log(
-          "🚀 ~ file: App.js ~ line 14 ~ .then ~ userCredential",
-          userCredential
-        );
-        // Signed in
-        // const user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="form">
-          <form onSubmit={handleSubmit}>
-            <div className="input-container">
-              <label>Email </label>
-              <input
-                type="text"
-                name="email"
-                required
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-              />
-            </div>
-            <div className="input-container">
-              <label>Password </label>
-              <input
-                type="password"
-                name="pass"
-                required
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-              />
-            </div>
-            <div className="button-container" onClick={handleSubmit}>
-              <input type="submit" />
-            </div>
-          </form>
-        </div>
-      </header>
+    <div>
+      <NavBar />
+      <Application />
+      <img
+        className="logoGBK2"
+        src="https://freesvg.org/img/1607600654curved-lines-abstract-svg.png"
+      />
     </div>
   );
 }
